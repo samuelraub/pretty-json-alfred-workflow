d7e56812f1cd7745e8d8f7200638963530e53c2f
Samuel Raub <samuelraub@MBP-von-Samuel.fritz.box>
2023-06-24 12:52:58 +0200

    analyse

--- lib/pj.rb
+++ lib/pj.rb
@@ -8,6 +8,10 @@ module PJ
       opts.on("-dDEPTH", "--indent-depthDEPTH", "Indent Depth") do |d|
        options[:d] = d 
       end
+
+      opts.on("-a", "--analyse", "Analyse") do |a|
+       options[:a] = a 
+      end
     end.parse!
     return options
   end
@@ -16,9 +20,7 @@ module PJ
 
     indent_depth = options[:d] ? options[:d].to_i : 2
 
-    json_string = query
-
-    parsed = JSON.parse json_string
+    parsed = JSON.parse query
 
     indent_string = ""
 
@@ -27,6 +29,12 @@ module PJ
     end
 
     JSON.pretty_generate parsed, indent: indent_string
+  end
+
+  def self.analyse(query)
+    parsed = JSON.parse query
+    puts "Number of elements: #{parsed.length}" if parsed.is_a? Array
+    puts "Number of keys: #{parsed.length}" if parsed.is_a? Hash
   end
 end
 
