7f9819782f3298a047e87f6e3f521debabf88b20
Samuel Raub <samuelraub@MBP-von-Samuel.fritz.box>
2023-06-24 12:32:11 +0200

    fix read from stdin

--- lib/pj.rb
+++ lib/pj.rb
@@ -1,17 +1,24 @@
 require 'json'
+require 'optparse'
 
 module PJ
+  def self.parse
+    options = {}
+    OptionParser.new do |opts|
+      opts.on("-dDEPTH", "--indent-depthDEPTH", "Indent Depth") do |d|
+       options[:d] = d 
+      end
+    end.parse!
+    return options
+  end
+
   def self.prettify(query, options)
 
-    indent_depth = options[:d].to_i || 2
+    indent_depth = options[:d] ? options[:d].to_i : 2
 
     json_string = query
 
-    begin
-      parsed = JSON.parse json_string
-    rescue JSON::ParserError
-      parsed = nil
-    end
+    parsed = JSON.parse json_string
 
     indent_string = ""
 
@@ -19,7 +26,7 @@ module PJ
       indent_string += " "
     end
 
-    JSON.pretty_generate parsed, indent: indent_string if parsed
+    JSON.pretty_generate parsed, indent: indent_string
   end
 end
 
